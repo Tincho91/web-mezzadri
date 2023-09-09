3c5455672b72d03ef842e5ef86e2b8a0d170c352
Martin Maiaru <tincho_91_15@hotmail.com>
2023-09-09 19:30:11 -0300

    Commit 4

--- layouts/layout.jsx
+++ layouts/layout.jsx
@@ -3,11 +3,6 @@
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 
-export const metadata = {
-  title: "Propiedades Mezzadri",
-  description: "Encuentra tu proximo hogar o inversion en Mar del Plata",
-};
-
 export default function RootLayout({ children }) {
   return (
     <div>
